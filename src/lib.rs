ca2004074d380f592907adbc8c92f667ec8d7191
Marc König <marc.koenig@tu-ilmenau.de>
2022-08-18 22:57:28 +0200

    Ab 10 uhr wird aufgeräumt

--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,47 @@
-pub mod neurons{
+pub mod neurons {
     use crate::{output::OutputFunction, state::StateFunction};
 
+    pub struct FireNeuron {}
 
-    pub struct FireNeuron{
-    }
-
-    pub struct LeakyNeuron{
-
+    pub struct LeakyNeuron {
         pub z: f64,
         pub y: f64,
         output_function: Box<dyn OutputFunction>,
         state_function: Box<dyn StateFunction>,
-
     }
 
-    pub struct StaticNeuron{
-
+    pub struct StaticNeuron {
         pub z: f64,
         pub y: f64,
         output_function: Box<dyn OutputFunction>,
         state_function: Box<dyn StateFunction>,
+    }
+}
 
+pub mod output {
+    pub trait OutputFunction {
+        fn calc(&self);
     }
 
-}
+    pub struct Id {}
 
-pub mod output{
-pub trait OutputFunction{
-
-    fn calc(&self);
-
-}
-
-pub struct Id{}
-
-pub struct Step{
-    pub t: f64,
-}
-
-pub struct Fermi{
-    pub c: f64,
-}
-
-pub struct Tangen{}
-
-pub struct RelU{}
-
-pub struct Gauss{}
-
-pub struct RBF{}
-
-pub struct EBF{}
-
-}
-
-pub mod state{
-    
-    pub trait StateFunction{
-
-    fn calc(&self);
-
+    pub struct Step {
+        pub t: f64,
     }
 
-pub struct ScalarProd{}
+    pub struct Fermi {
+        pub c: f64,
+    }
 
-pub struct EuklidDist{}
+    pub struct Tangen {}
 
-pub struct MaxDist{}
+    pub struct RelU {}
 
-pub struct MinDist{}
+    pub struct Gauss {}
 
-pub struct ManhattanDist{}
+    pub struct RBF {}
 
-pub struct MahalanobisDist{}
-
+    pub struct EBF {}
 }
+
+pub mod state;
---
+++ src/state/mod.rs
@@ -0,0 +1,18 @@
+use nalgebra as alg;
+
+mod scalar_prod;
+pub use scalar_prod::*;
+
+pub trait StateFunction {
+    fn calc(&self, weight: alg::DVector<f64>, input: alg::DVector<f64>) -> f64;
+}
+
+pub struct EuklidDist {}
+
+pub struct MaxDist {}
+
+pub struct MinDist {}
+
+pub struct ManhattanDist {}
+
+pub struct MahalanobisDist {}
