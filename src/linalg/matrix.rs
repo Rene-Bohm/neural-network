543d9f67dcf12c5557669a48ac0b59dca6b8e63e
Rene-Bohm <rene.kassbohm@gmail.com>
2022-10-09 21:56:58 +0200

    added Matrix functionalities

--- src/linalg/matrix.rs
+++ src/linalg/matrix.rs
@@ -1,6 +1,8 @@
 #![allow(unused)]
 
-use core::fmt;
+use core::{fmt, panic};
+use std::collections::btree_map::Iter;
+use std::fmt::Display;
 use std::ops::{Add, Mul};
 use std::ops::{Index, IndexMut};
 use std::vec;
@@ -15,7 +17,7 @@ enum dataTypes{
 
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Matrix{
 
     rows: usize,
@@ -38,6 +40,12 @@ impl Matrix{
         }
 
         Matrix { rows: row, columns: column, entries: vec }
+
+    }
+
+    pub fn dimension(self) -> (usize, usize){
+
+        (self.rows, self.columns)
 
     }
 
@@ -103,29 +111,42 @@ impl Matrix{
 
     pub fn dot(self, input: &Matrix) -> Matrix {
 
+
+        let dim1 = self.clone().dimension();
+        let dim2 = input.clone().dimension();
         let mut vec:Vec<Vec<f64>> = Vec::new();
-        
-        for _i in 0..self.rows {
 
-            vec.push(vec![0.0; input.columns]);
-            
-        }
+        if(dim1.1 != dim2.0){
 
-        for i in 0..self.rows{
+            panic!("The Coloumn size of the left Matrix: {}x{} \n 
+                    doesn't coincide with \n
+                    The Row size of the right Matrix {}x{} \n
+                    {} != {}", 
+                    dim1.0,dim1.1, dim2.0 , dim2.1, dim1.1, dim2.0);
 
-            for j in 0..input.columns{
+        }else{
+            for _i in 0..self.rows {
 
-                let mut sum = 0.0;
-
-                for k in 0..input.rows{
-                    sum += self.entries[i][k] * input.entries[k][j];
-                }
-                vec[i][j] = sum;
+                vec.push(vec![0.0; input.columns]);
+                
             }
 
-        }
+            for i in 0..self.rows{
 
-        Matrix { rows: self.rows, columns: input.columns, entries: vec }
+                for j in 0..input.columns{
+
+                    let mut sum = 0.0;
+
+                    for k in 0..input.rows{
+                        sum += self.entries[i][k] * input.entries[k][j];
+                    }
+                    vec[i][j] = sum;
+                }
+
+            }
+
+            Matrix { rows: self.rows, columns: input.columns, entries: vec }
+        }
 
     }
 
@@ -167,13 +188,23 @@ impl Matrix{
         }
     }
 
+    pub fn map(self, f: fn()) -> Matrix{
+    todo!()
+
+    }
+
 }
 
 impl Add for Matrix{
     type Output = Matrix;
 
     fn add(self, rhs: Matrix) -> Self::Output {
+
+        let dim1 = self.clone().dimension();
+        let dim2 = rhs.clone().dimension();
         
+        if(dim1 == dim2){
+
         let mut vec:Vec<Vec<f64>> = Vec::new();
         
         for _i in 0..self.rows{
@@ -193,6 +224,12 @@ impl Add for Matrix{
         }
 
         Matrix { rows: self.rows, columns: self.columns, entries: vec }
+
+        }else{
+
+            
+            panic!("not the same dimension for addition: {}x{} != {}x{}", dim1.0,dim1.1, dim2.0 , dim2.1);
+        }
 
     }
 }
@@ -227,7 +264,7 @@ impl std::ops::Mul<Matrix> for f64 {
 
 impl IndexMut<usize> for Matrix {
     
-    fn index_mut<'a>(&'a mut self, index: usize) -> &'a mut Vec<f64> {
+    fn index_mut<'a>(&'a mut self, index: usize) -> &'a mut [f64] {
 
         &mut self.entries[index]
     }
@@ -235,20 +272,47 @@ impl IndexMut<usize> for Matrix {
 
 impl Index<(usize, usize)> for Matrix {
     type Output = f64;
-    fn index(&self, index: (usize, usize)) -> &Self::Output {
+    fn index(&self, index: (usize, usize)) -> &Self::Output{
       &self.entries[index.0][index.1]  
     }
 }
 
 impl Index<usize> for Matrix {
-    type Output = Vec<f64>;
+    type Output = [f64];
 
-    fn index(&self, index: usize) -> &Vec<f64> {
+    fn index(&self, index: usize) -> &[f64] {
         
         &self.entries[index]
 
     }
 }
+
+impl Display for Matrix
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Matrix<{}, {}> {{", self.rows, self.columns)?;
+
+        for i in 0..self.rows {
+            for j in 0..self.columns {
+                write!(f, " {}", self[(i, j)])?;
+            }
+            writeln!(f)?;
+        }
+
+        write!(f, "}}")
+    }
+}
+
+/* 
+impl Iterator for Matrix{
+    type Item = f64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        
+
+    }
+}
+*/
 
 #[cfg(test)]
 mod test {
@@ -257,20 +321,7 @@ mod test {
     #[test]
     fn do_test(){
 
-        let mut m1 = Matrix::new(3, 2);
-        m1.visualize();
-        println!("\n");
-        
-        let input = vec![1.0 , 2.0, 3.0, 4.0, 5.0, 6.0,];
-        m1.insert(input);
-        m1.visualize();
-        println!("\n");
-        
-        let m2 = m1.T();
-        m2.visualize();
-        println!("\n");
-
-        let mut m3 = Matrix::new(2, 2);
+        let mut m3 = Matrix::new(2, 4);
         m3.insert(vec![1.0,2.0, 3.0,4.0]);
 
         let mut m4 = Matrix::new(2, 2);
@@ -280,33 +331,102 @@ mod test {
         m5.visualize();
         println!("\n");
 
-        let m6 = 3.0 * m1;
+        let m6 = 3.0 * m5;
         m6.visualize();
         println!("\n");
 
-
-        //-----------------------------------
-        let mut m7 = Matrix::new(2, 3);
-        m7.insert(vec![1.0 , 2.0, 3.0, 4.0, 5.0, 6.0,]);
-        m7.visualize();
-        println!("\n");
-
-        let mut m8 = Matrix::new(3, 2);
-        m8.insert(vec![1.0 , 2.0, 3.0, 4.0, 5.0, 6.0,]);
-        m8.visualize();
-        println!("\n");
-
-        let m9 = m7.dot(&m8);
-        m9.visualize();
-        println!("\n");
         //-----------------------------------
 
-        let m10 = Matrix::eye(4);
+        let mut m10 = Matrix::eye(4);
         m10.visualize();
         println!("\n"); 
         //-----------------------------------
 
+        m10[0][0] = 42.0;
+
         println!("{:?}", m10[0][0]);
+        println!("{}", m10);
+
+    }
+
+    #[test]
+    fn T(){
+
+        let mut m1 = Matrix::new(3, 2);
+        m1.insert(vec![1.0 , 2.0, 3.0, 4.0, 5.0, 6.0]);
+  
+        let m2 = m1.T();
+
+        let mut m3 = Matrix::new(2, 3);
+        m3.insert(vec![1.0 , 3.0, 5.0, 2.0, 4.0, 6.0]);
+ 
+
+        assert_eq!(m2, m3);
+
+        let mut m01 = Matrix::new(1, 1);
+        m1.insert(vec![1.0]);
+        
+        let m02 = m01.T();
+
+        assert_eq!(m01, m02);
+
+    }
+
+    #[test]
+    fn dot(){
+
+        let mut m1 = Matrix::new(2, 3);
+        m1.insert(vec![1.0 , 2.0, 3.0, 4.0, 5.0, 6.0,]);
+
+        let mut m2 = Matrix::new(3, 2);
+        m2.insert(vec![1.0 , 2.0, 3.0, 4.0, 5.0, 6.0,]);
+
+        let mut mdot =  Matrix::new(2, 2);
+        mdot.insert(vec![22.0 , 28.0, 49.0, 64.0]);
+
+        let mtest = m1.dot(&m2);
+        
+        assert_eq!(mdot, mtest);
+
+    }
+
+    #[test]
+    fn add(){
+
+        let mut m1 = Matrix::new(2, 2);
+        m1.insert(vec![1.0,2.0, 3.0,4.0]);
+
+        let mut m2 = Matrix::new(2, 2);
+        m2.insert(vec![5.0,6.0, 7.0,8.0]);
+
+        let m3 = m1 + m2;
+
+        let mut m4 = Matrix::new(2, 2);
+        m4.insert(vec![6.0,8.0,10.0,12.0]);
+
+        assert_eq!(m3, m4);
+
+    }
+
+    #[test]
+    fn mul(){
+
+
+
+
+    }
+
+    #[test]
+    fn sub(){
+
+
+
+    }
+
+    #[test]
+    fn index(){
+
+
 
     }
 
