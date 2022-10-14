98e2acb27abe963e1366c01f0b58eebe6b7399eb
Rene-Bohm <rene.kassbohm@gmail.com>
2022-10-14 16:43:57 +0200

    inlcuded matrix functions with loss, derivative and accuracy calculations

--- src/loss_calc/accuracy.rs
+++ src/loss_calc/accuracy.rs
@@ -1,8 +1,8 @@
 use crate::prelude::*;
 use f64;
 
-/* 
-pub fn batch_accuracy(actual: Vec<usize>, expected: Matrix) -> f64 {
+
+pub fn batch_accuracy(actual: &Vec<usize>, expected: &Matrix) -> f64 {
     
     let dimension = expected.dimension();
 
@@ -10,33 +10,48 @@ pub fn batch_accuracy(actual: Vec<usize>, expected: Matrix) -> f64 {
         println!("actual.len(): {}, dimension: {:?}", actual.len(), dimension);
 
         panic!("Batch Accuracy cant be calculated")
-    } else {
-        let mut max = expected[0][0];
 
-        for i in expected.into_iter() {
-            if i > max {
-                max = i;
+    } else {
+
+        let mut max_index = actual[0];
+
+        for i in actual.iter(){
+
+            if *i > max_index {
+                max_index = *i;
             }
         }
+        if(max_index >= dimension.1){
 
-        if expected[actual] != max {
-            0.0
-        } else {
-            1.0
+            panic!("Batch Accuracy cant be calculated, index is indexing value outside of the coloumn")
+
+        }else{
+
+            let samples_num = actual.len() as f64;
+
+            let mut sum = 0.0;
+
+            for i in 0..dimension.0{
+                
+                let mut row_max = expected[i][0];
+                
+                for i in expected[i].into_iter() {
+                    if *i > row_max {
+                        row_max = *i;
+                    }
+                }
+
+                sum += if expected[i][actual[i]] != row_max {
+                        1.0
+                    } else {
+                        0.0
+                    };
+            }
+            sum / samples_num
         }
-
-        let len = expected.len() as f64;
-        let mut sum = 0.0;
-        for i in 0..expected.len() {
-            sum += accuracy(actual[i], expected[i].clone());
-        }
-
-        sum / len
-        
-    
     }
 }
-*/
+
 pub fn accuracy(actual: usize, expected: Vec<f64>) -> f64 {
     let mut max = expected[0];
 
@@ -47,9 +62,9 @@ pub fn accuracy(actual: usize, expected: Vec<f64>) -> f64 {
     }
 
     if expected[actual] != max {
-        0.0
-    } else {
         1.0
+    } else {
+        0.0
     }
 }
 
@@ -94,13 +109,48 @@ mod test {
 
         let loss = avg_batch_accuracy(actual, expected);
 
-        assert_eq!(1.0, loss);
+        assert_eq!(0.0, loss);
 
         let actual: Vec<usize> = vec![1];
         let expected: Vec<Vec<f64>> = vec![vec![0.7, 0.0, 0.2]];
 
         let loss = avg_batch_accuracy(actual, expected);
 
-        assert_eq!(0.0, loss);
+        assert_eq!(1.0, loss);
+    }
+
+    #[test]
+    fn matrix_batch(){
+
+        let actual: Vec<usize> = vec![0, 1, 1];
+        let expected= Matrix::from(vec![
+            vec![0.7, 0.0, 0.2],
+            vec![0.1, 0.5, 0.4],
+            vec![0.02, 0.9, 0.08],
+        ]);
+
+        let acc = batch_accuracy(&actual, &expected);
+
+        assert_eq!(1.0, acc);
+
+        let actual: Vec<usize> = vec![1];
+        let expected = Matrix::from(vec![vec![0.7, 0.0, 0.2]]);
+
+        let acc = batch_accuracy(&actual, &expected);
+
+        assert_eq!(0.0, acc);
+
+        let actual: Vec<usize> = vec![1, 2, 0];
+        let expected= Matrix::from(vec![
+            vec![0.7, 0.0, 0.2],
+            vec![0.1, 0.5, 0.4],
+            vec![0.02, 0.9, 0.08],
+        ]);
+
+        let acc = batch_accuracy(&actual, &expected);
+
+
+        assert_eq!(0.0, acc);
+
     }
 }
--- src/loss_calc/loss.rs
+++ src/loss_calc/loss.rs
@@ -1,11 +1,54 @@
 use crate::prelude::*;
 use f64;
 
+pub fn batch_loss(actual: &Vec<usize>, expected: &Matrix) -> f64 {
+    
+    let dimension = expected.dimension();
+
+    if actual.len() != dimension.0 || dimension.0 == 0 || dimension.1 == 0 {
+        println!("actual.len(): {}, dimension: {:?}", actual.len(), dimension);
+
+        panic!("Batch loss cant be calculated")
+
+    } else {
+
+        let mut max_index = actual[0];
+
+        for i in actual.iter(){
+
+            if *i > max_index {
+                max_index = *i;
+            }
+        }
+        if(max_index >= dimension.1){
+
+            panic!("Batch loss cant be calculated, index is indexing value outside of the coloumn")
+
+        }else{
+
+            let samples_num = actual.len() as f64;
+
+            let mut sum = 0.0;
+
+            for i in 0..dimension.0{
+                
+                sum += if expected[i][actual[i]] == 0.0{
+                        -f64::ln(1e-7)
+                    } else {
+                        -f64::ln(expected[i][actual[i]] - 1e-7)
+                    };
+            }
+            sum / samples_num
+        }
+    }
+}
+
+
 pub fn loss(actual: usize, expected: Vec<f64>) -> f64 {
     if expected[actual] == 0.0 {
         -f64::ln(1e-7)
     } else {
-        -f64::ln(expected[actual])
+        -f64::ln(expected[actual] - 1e-7)
     }
 }
 
@@ -17,6 +60,49 @@ pub fn avg_batch_loss(actual: Vec<usize>, expected: Vec<Vec<f64>>) -> f64 {
     }
 
     sum / len
+}
+
+//---------------------------------------------------------------------------
+
+pub fn batch_derivative(softmax_output: &mut Matrix, expected_class_index: &Vec<usize>) -> Matrix{
+
+    let dimension = softmax_output.dimension();
+
+    if expected_class_index.len() != dimension.0 || dimension.0 == 0 || dimension.1 == 0 {
+        println!("expected_class_index.len(): {}, dimension: {:?}", expected_class_index.len(), dimension);
+
+        panic!("Batch derivative cant be calculated")
+
+    } else {
+
+        let mut max_index = expected_class_index[0];
+
+        for i in expected_class_index.iter(){
+
+            if *i > max_index {
+                max_index = *i;
+            }
+        }
+        if(max_index >= dimension.1){
+
+            panic!("Batch derivative cant be calculated, index is indexing value outside of the coloumn")
+
+        }else{
+
+            let samples_num = expected_class_index.len() as f64;
+
+            for i in 0..dimension.0{
+                
+                softmax_output[i][expected_class_index[i]] -= 1.0;
+
+            }
+
+            let m1 = softmax_output.clone().map(|x| x / dimension.0 as f64);
+            
+            m1
+        }
+    }
+
 }
 
 pub fn softmax_cross_derivative(
@@ -44,7 +130,9 @@ pub fn batch_softmax_cross_derivative(
 #[cfg(test)]
 mod test {
 
-    use crate::loss_calc::*;
+    use loss::batch_derivative;
+
+    use crate::{loss_calc::*, linalg::Matrix};
 
     #[test]
     fn do_works() {
@@ -89,6 +177,39 @@ mod test {
         let loss = avg_batch_loss(actual, expected);
 
         assert_eq!(16.11809565095832, loss);
+    }
+
+    #[test]
+    fn matrix_batch(){
+
+        let actual: Vec<usize> = vec![0, 1, 1];
+        let expected= Matrix::from(vec![
+            vec![0.7, 0.1, 0.2],
+            vec![0.1, 0.5, 0.4],
+            vec![0.02, 0.9, 0.08],
+        ]);
+
+        let loss = batch_loss(&actual, &expected);
+        
+        assert_eq!(0.3850610313749314, loss);
+
+        let actual: Vec<usize> = vec![1];
+        let expected = Matrix::from(vec![vec![0.7, 0.0, 0.2]]);
+
+        let loss = batch_loss(&actual, &expected);
+
+        assert_eq!(16.11809565095832, loss);
+
+        let actual: Vec<usize> = vec![1, 2, 0];
+        let expected= Matrix::from(vec![
+            vec![0.7, 0.0, 0.2],
+            vec![0.1, 0.5, 0.4],
+            vec![0.02, 0.9, 0.08],
+        ]);
+
+        let loss = batch_loss(&actual, &expected);
+        assert_eq!(6.982138212757718, loss);
+
     }
 
     #[test]
@@ -141,4 +262,41 @@ mod test {
             ]
         );
     }
+
+    #[test]
+    fn batch_derivatives() {
+
+        let actual: Vec<usize> = vec![0, 1, 1];
+        let mut m1 = Matrix::from(vec![
+            vec![0.7, 0.1, 0.2],
+            vec![0.1, 0.5, 0.4],
+            vec![0.02, 0.9, 0.08],
+        ]);
+
+        let result = batch_derivative(&mut m1, &actual);
+
+        println!("{:?}", &result);
+
+        assert_eq!(
+            result,
+            Matrix::from(vec![
+                vec![
+                    -0.10000000000000002,
+                    0.03333333333333333,
+                    0.06666666666666667
+                ],
+                vec![
+                    0.03333333333333333,
+                    -0.16666666666666667,
+                    0.13333333333333333
+                ],
+                vec![
+                    0.006666666666666667,
+                    -0.033333333333333326,
+                    0.02666666666666667
+                ]
+            ]
+        ));
+    }
+
 }
